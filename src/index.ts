256eaaf99eb89f38e62efb7d5a8eeffb85962f59
jellyfish1129 <120287208+jellyfish1129@users.noreply.github.com>
2024-10-23 03:52:11 +0900

    fix: delete unnecessary cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { Hono } from "hono";
 import {todos} from "./todos/api";
 import { basicAuth } from "hono/basic-auth";
-import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { requestId } from "hono/request-id";
 
 const app = new Hono();
 app.use("*", logger());
-app.use("*", cors());
 app.use("*", requestId());
 app.use("*", (c, next) => {
 	console.log(`Request ID: ${c.get("requestId")}`);
